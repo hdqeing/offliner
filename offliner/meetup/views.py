f0e5b09feaa04d74be6cde4ed4590ee7472610ee
dingqing.he <dingqing.he@stud.uni-goettingen.de>
2023-01-18 23:27:46 +0100

    typo

--- offliner/meetup/views.py
+++ offliner/meetup/views.py
@@ -6,7 +6,7 @@ def index(request):
     return HttpResponse("Hello, welcome to the offliner app, a place for meet up with people offline")
 
 def register_user(request):
-    if request.method =='post':
+    if request.method =='POST':
         form = UserRegistrationForm(request.POST)
         if form.is_valid():
             form.save()
--- offliner/offliner/settings.py
+++ offliner/offliner/settings.py
@@ -76,11 +76,10 @@ WSGI_APPLICATION = 'offliner.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'HOST': 'offlinerdb.mysql.database.azure.com',
-        'NAME': 'offlinerdb',
-        'USER': 'offlinerDB',
-        'PASSWORD': '*',
+
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+
     }
 }
 
