fb93a25f1181c0ff11e0befcfe2c7abdbe43303e
dingqing.he <dingqing.he@stud.uni-goettingen.de>
2023-01-18 22:24:01 +0100

    add login function

---
+++ offliner/meetup/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .forms import UserRegistrationForm
+
+def index(request):
+    return HttpResponse("Hello, welcome to the offliner app, a place for meet up with people offline")
+
+def register_user(request):
+    if request.method =='post':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = UserRegistrationForm()
+    return render(request=request, template_name='meetup/register.html', context={'register_form':form})
+
